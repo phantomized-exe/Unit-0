371898c11cd5cf5d4ab35a6fc83465c36e422861
phantomized <boon.atticus@gmail.com>
2025-02-04 09:56:15 -0800

    update

--- Lesson-2/lesson2.py
+++ Lesson-2/lesson2.py
@@ -1,5 +1,23 @@
-def main():
-    price = float(input("What is the price? "))
-    print(round(price,2))
+def find_average_price(price1,price2):
+    return (price1+price2)/2
 
-main()
+def cube_volume(side_length):
+    return side_length**3
+
+
+
+# exception for not printing inside functions
+def main():
+    '''price1 = float(input("What is the price? "))
+    price2 = float(input("What is the next price? "))
+    avg_price = find_average_price(price1,price2)
+    print(f"The average price of ${price1} and ${price2} is ${avg_price:.2f}.")
+    '''
+    print(f"Side length 3, volume {cube_volume(3)}")
+    print(f"Side length 7, volume {cube_volume(7)}")
+    print(f"Side length 10, volume {cube_volume(10)}")
+
+
+
+if __name__ == '__main__':
+    main()
