f7e4f3e69684660f3d3b89a513d75d680aa87767
phantomized <boon.atticus@gmail.com>
2025-02-25 09:24:03 -0800

    fixed postal code

--- Projects/canadian-postal-code.py
+++ Projects/canadian-postal-code.py
@@ -1,5 +1,21 @@
-provinces = {"A":"Newfoundland","B":"Nova Scotia","C":"Prince Edward Island","E":"New Brunswick","G":"Quebec","H":"Quebec","J":"Quebec","K":"Ontario","L":"Ontario","M":"Ontario","N":"Ontario","P":"Ontario","R":"Manitoba","S":"Saskatchewan","T":"Alberta","V":"British Columbia","X":"Nunavut or Northwest Territories","Y":"Yukon"}
-postal_code = input("Enter Canadian postal code: ")
-print("test")
-if postal_code.upper() not in "ABCEGHJKLMNPRSTVXY" or len(postal_code) != 6:
-    print("Invalid postal code")
+def main():
+    provinces = {"A":"Newfoundland","B":"Nova Scotia","C":"Prince Edward Island",
+    "E":"New Brunswick","G":"Quebec","H":"Quebec","J":"Quebec",
+    "K":"Ontario","L":"Ontario","M":"Ontario","N":"Ontario",
+    "P":"Ontario","R":"Manitoba","S":"Saskatchewan","T":"Alberta",
+    "V":"British Columbia","X":"Nunavut or Northwest Territories","Y":"Yukon"
+    }
+    postal_code = input("Enter Canadian postal code: ")
+    if postal_code[0].upper() not in "ABCEGHJKLMNPRSTVXY" or len(postal_code) != 6:
+        print("Invalid postal code")
+        main()
+    for i in provinces.keys():
+        if postal_code[0].upper() == i:
+            if postal_code[1] == 0:
+                print(f"The postal code is for a rural address in {provinces[i]}")
+                return
+            else:
+                print(f"The postal code is for an urban address in {provinces[i]}")
+                return
+if __name__ == "__main__":
+    main()
